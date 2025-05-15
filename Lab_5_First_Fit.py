ca6524c074c1365f1e5d58b57a0b97de2ac1e3ec
Raga <149788035+ragavenron7@users.noreply.github.com>
2025-05-15 09:42:15 +0800

    Add Lab_5_First_Fit.py

---
+++ Lab_5_First_Fit.py
@@ -0,0 +1,45 @@
+def first_fit_memory_allocation(blocks, files):
+    n_blocks = len(blocks)
+    n_files = len(files)
+    allocation = [-1] * n_files
+    fragment = [None] * n_files
+    block_sizes = blocks[:]
+
+    for i in range(n_files):
+        for j in range(n_blocks):
+            if block_sizes[j] >= files[i]:
+                allocation[i] = j
+                fragment[i] = block_sizes[j] - files[i]
+                block_sizes[j] = -1  # Mark block as used
+                break
+
+    print("\nFile No\tFile Size\tBlock No\tBlock Size\tFragment")
+    for i in range(n_files):
+        print(f"{i + 1}\t{files[i]}\t\t", end="")
+        if allocation[i] != -1:
+            block_no = allocation[i] + 1
+            block_size = blocks[allocation[i]]
+            print(f"{block_no}\t\t{block_size}\t\t{fragment[i]}")
+        else:
+            print("-\t\t-\t\tNot Allocated")
+
+
+# Input Section
+blocks = []
+files = []
+
+n_blocks = int(input("Enter the number of blocks: "))
+n_files = int(input("Enter the number of files: "))
+
+print("\nEnter the size of the blocks:")
+for i in range(n_blocks):
+    size = int(input(f"Block {i+1}: "))
+    blocks.append(size)
+
+print("\nEnter the size of the files:")
+for i in range(n_files):
+    size = int(input(f"File {i+1}: "))
+    files.append(size)
+
+# Run First-Fit Allocation
+first_fit_memory_allocation(blocks, files)
